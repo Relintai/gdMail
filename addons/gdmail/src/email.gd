992bffd54b7a67229e5d4debcdac87aa193b7dd9
Relintai <relintai@protonmail.com>
2024-01-10 12:21:06 +0100

    Merged in InternetAddress.

--- addons/gdmail/src/email.gd
+++ addons/gdmail/src/email.gd
@@ -1,23 +1,27 @@
 extends Reference
 class_name Email
 
-var from: InternetAddress = null
+var from_address: String = ""
+var from_personal: String = ""
+
 #Array[InternetAddress] 
 var to: Array = []
 var subject: String = ""
 var body: String = ""
 
-func _init(from: InternetAddress, to: Array = [], subject: String = "", body: String = "") -> void:
-	set_sender(from)
+func _init(address: String, personal: String = "", to: Array = [], subject: String = "", body: String = "") -> void:
+	self.address = address
+	self.personal = personal
 	set_recipients(to)
 	set_body(body)
 	set_subject(subject)
 
-func set_sender(from: InternetAddress) -> void:
-	self.from = from
+func set_sender(address: String, personal: String = "") -> void:
+	self.address = address
+	self.personal = personal
 
-func add_recipient(to: InternetAddress) -> void:
-	self.to.append(to)
+func add_recipient(address: String, personal: String = "") -> void:
+	self.to.append([address, personal])
 
 func set_recipients(to: Array) -> void:
 	self.to = to
@@ -29,5 +33,4 @@ func set_body(body: String) -> void:
 	self.body = body
 
 func _to_string() -> String:
-	return ("From: %s\nTo: %s\nSubject: %s\n\n%s\n" % [from, ",".join(to), subject, body])
-
+	return ("From: %s <%s>\nTo: %s\nSubject: %s\n\n%s\n" % [from_personal if not from_personal.empty() else from_address.split("@")[0], from_address, ",".join(to), subject, body])
